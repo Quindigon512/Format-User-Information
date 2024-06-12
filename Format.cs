e21b302a50608e1ba3d63c8b1573183f01e0dc35
Quindigon512 <128196417+Quindigon512@users.noreply.github.com>
2024-06-12 12:49:35 -0400

    Add files via upload

---
+++ Format.cs
@@ -0,0 +1,84 @@
+﻿//----------------------------------------------
+// Author:     Quinn Trate
+// Date:       February 3, 2024
+// Purpose:    Enter in User Information
+//             and the Program will Format
+//             the Information
+//----------------------------------------------
+
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatUsers
+{
+    class Format
+    {
+        static void Main(string[] args)
+        {
+            // Initial Variables
+            bool flag = true;
+            int counter = 0;
+            string answer = "";
+            List<User> arrUser = new List<User>();
+
+            while (flag)
+            {
+                // Get User Information
+                Console.WriteLine("Please Enter User Number " + (counter + 1) + "'s Information");
+                Console.WriteLine("First Name:");
+                string strFirstName = Console.ReadLine();
+                Console.WriteLine("Last Name:");
+                string strLastName = Console.ReadLine();
+                Console.WriteLine("Age:");
+                int intAge = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Height (in Inches):");
+                int intHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Weight (in Pounds):");
+                double dblWeight = Convert.ToDouble(Console.ReadLine());
+
+                // Creates New User Object and Adds it to the ArrayList
+                arrUser.Add(new User(strFirstName, strLastName, intAge, intHeight, dblWeight));
+
+                // Asks for More Users
+                Console.WriteLine("\nIs There Another User you Need to Enter? (Y/N)");
+                answer = Console.ReadLine();
+                Console.WriteLine("");
+
+                // Ensures Correct Input
+                while (answer != "Y" && answer != "y" && answer != "N" && answer != "n")
+                {
+                    Console.WriteLine("Error. Please Enter Y/N");
+                    answer = Console.ReadLine();
+                }
+
+                // Increase the User Counter
+                if (answer == "Y" || answer == "y")
+                    counter++;
+                // End While
+                else if (answer == "N" || answer == "n")
+                {
+                    flag = false;
+                    Console.WriteLine("");
+                }
+            }
+
+            Console.Clear();
+
+            // Print all Formatted User Information
+            Console.WriteLine("User Information\n");
+            for (int i = 0; i < arrUser.Count; i++)
+            {
+                arrUser[i].print();
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,78 @@
+﻿//----------------------------------------------
+// Author:     Quinn Trate
+// Date:       February 3, 2024
+// Purpose:    Constructor Class for User
+//             Objects. Includes Print
+//             Function for Formatting
+//----------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatUsers
+{
+    internal class User
+    {
+        // User Variables
+        private string strFirstName;
+        private string strLastName;
+        private int intAge;
+        private int intHeight;
+        private double dblWeight;
+
+        // User Constructor
+        public User(string strFirstName, string strLastName, int intAge, int intHeight, double dblWeight)
+        {
+            this.strFirstName = strFirstName;
+            this.strLastName = strLastName;
+            this.intAge = intAge;
+            this.intHeight = intHeight;
+            this.dblWeight = dblWeight;
+        }
+
+        // Converts the Height from Inches to Feet
+        public static string convertHeight(int intHeight)
+        {
+            string strInches = "";
+            string strFeet = "";
+            string strNewHeight = "";
+
+            int intInches = intHeight % 12;
+            int intFeet = (intHeight - intInches) / 12;
+
+            if (intInches == 1)
+                strInches = "Inch";
+            else
+                strInches = "Inches";
+
+            if (intFeet == 1)
+                strFeet = "Foot";
+            else
+                strFeet = "Feet";
+
+            if (intInches == 0 && intFeet == 0)
+                strNewHeight = "0 Feet, 0 Inches";
+            else if (intInches == 0)
+                strNewHeight = intFeet + " " + strFeet;
+            else if (intFeet == 0)
+                strNewHeight = intInches + " " + strInches;
+            else
+                strNewHeight = intFeet + " " + strFeet + ", " + intInches + " " + strInches;
+
+            return strNewHeight;
+        }
+
+        // Prints the Information
+        public void print()
+        {
+            Console.WriteLine("Name: " + strFirstName + " " + strLastName);
+            Console.WriteLine("Age: " + intAge + " Years Old");
+            Console.WriteLine("Height: " + convertHeight(intHeight));
+            Console.WriteLine("Weight: " + dblWeight + " Pounds");
+            Console.WriteLine("");
+        }
+    }
+}
